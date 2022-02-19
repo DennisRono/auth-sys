55364226ff398ae9b5b5f635db29783553667eeb
unknown <finnneron1@gmail.com>
2022-02-19 14:03:03 +0300

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,81 @@
-import React, { Component } from 'react'
+import React from 'react'
 import GoogleLogin from 'react-google-login'
 import './css/auth.css'
 
-export class App extends Component {
+const App = () => {
 
-  responseGoogle = (response) => {
+  const responseGoogle = (response) => {
     console.log(response);
     console.log(response.profileObj);
   }
 
-  showLogin = () => {
+  function showLogin() {
 
-  } 
-
-  render() {
-    return (
-      <div>
-        <div className="authWrapper">
-          <div className="authContainer">
-            <div className="authFlex">
-              <div className="authPlay">
-                <div className="authReg">
-                  <div className="authRegFlex">
-                    <div className="authRegForm">
-                      <div className="authControls">
-                        <div className="thecontrols">
-                          <button className="showregform" onClick={()=>{showLogin()}}>Register</button>
-                          <button className="showlogform">Login</button>
-                        </div>
+  }
+  return (
+    <div>
+      <div className="authWrapper">
+        <div className="authContainer">
+          <div className="authFlex">
+            <div className="authPlay">
+              <div className="authReg">
+                <div className="authRegFlex">
+                  <div className="authRegForm">
+                    <div className="authControls">
+                      <div className="thecontrols">
+                        <button className="showregform" onClick={()=>{showLogin()}}>Register</button>
+                        <button className="showlogform">Login</button>
                       </div>
-                      <form className="reg-form" autoComplete="off">
-                          <p className="err-box"></p>
-                          <input type="hidden" autoComplete="new-password" name="hidden" />
-                          <div className="user-input-wrp">
-                              <br/>
-                              <input type="text" className="inputText" name="fullname" required/>
-                              <span className="floating-label">Full Name</span>
-                          </div>
-                          <div className="user-input-wrp">
-                              <br/>
-                              <input id="id-input" type="text" className="inputText" name="phoneno"   required/>
-                              <span className="floating-label">Phone number (optional)</span>
-                          </div>
-                          <span id="id-err"></span>
-                          <div className="user-input-wrp">
-                              <br/>
-                              <input type="email" className="inputText" name="email"   required/>
-                              <span className="floating-label">Email Address</span>
-                          </div>
-                          <div className="user-input-wrp"style={{ marginBottom: "5px !important" }} >
-                              <br/>
-                              <input type="password" className="inputText" name="password" required/>
-                              <span className="floating-label">Password</span>
-                          </div>
-                          <div className="user-input-wrp">
-                              <br/>
-                              <input type="password" className="inputText" name="cpassword" required/>
-                              <span className="floating-label">Confirm Password</span>
-                          </div><br/>
-                          <input type="submit" className="submit-btn" value="Register" name="submit"/>
-                      </form>
-                      <GoogleLogin
-                        className="google-login"
-                        clientId="19488264174-ph00unj3grek6shl3sq5euc9ru0fq2l0.apps.googleusercontent.com"
-                        buttonText="Login with Google"
-                        onSuccess={this.responseGoogle}
-                        onFailure={this.responseGoogle}
-                        cookiePolicy={'single_host_origin'}
-                      />
                     </div>
-                    <div className="authRegImage"></div>
+                    <form className="reg-form" autoComplete="off">
+                        <p className="err-box"></p>
+                        <input type="hidden" autoComplete="new-password" name="hidden" />
+                        <div className="user-input-wrp">
+                            <br/>
+                            <input type="text" className="inputText" name="fullname" required/>
+                            <span className="floating-label">Full Name</span>
+                        </div>
+                        <div className="user-input-wrp">
+                            <br/>
+                            <input id="id-input" type="text" className="inputText" name="phoneno"   required/>
+                            <span className="floating-label">Phone number (optional)</span>
+                        </div>
+                        <span id="id-err"></span>
+                        <div className="user-input-wrp">
+                            <br/>
+                            <input type="email" className="inputText" name="email"   required/>
+                            <span className="floating-label">Email Address</span>
+                        </div>
+                        <div className="user-input-wrp"style={{ marginBottom: "5px !important" }} >
+                            <br/>
+                            <input type="password" className="inputText" name="password" required/>
+                            <span className="floating-label">Password</span>
+                        </div>
+                        <div className="user-input-wrp">
+                            <br/>
+                            <input type="password" className="inputText" name="cpassword" required/>
+                            <span className="floating-label">Confirm Password</span>
+                        </div><br/>
+                        <input type="submit" className="submit-btn" value="Register" name="submit"/>
+                    </form>
+                    <GoogleLogin
+                      className="google-login"
+                      clientId="19488264174-ph00unj3grek6shl3sq5euc9ru0fq2l0.apps.googleusercontent.com"
+                      buttonText="Login with Google"
+                      onSuccess={responseGoogle}
+                      onFailure={responseGoogle}
+                      cookiePolicy={'single_host_origin'}
+                    />
                   </div>
+                  <div className="authRegImage"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
